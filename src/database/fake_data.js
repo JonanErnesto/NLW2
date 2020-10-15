bf5046eac78b8d422ce8e80392f57958b92c523e
JonanErnesto <jonanernestojr@hotmail.com>
2020-10-14 21:03:19 -0300

    Primeiro commit

---
+++ src/database/fake_data.js
@@ -0,0 +1,36 @@
+// Dados
+const proffys = [
+    { 
+        name: "Jonan Ernesto",
+        avatar: "https://avatars0.githubusercontent.com/u/69017629?s=400&u=0dd09a9dd757b1c058bd89aedc32a638ff9c1d69&v=4",
+        whatsapp:"54654461665",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject:"Química",
+        cost:"15",
+        weekday: [0], 
+        time_from: [720], 
+        time_to: [1220] 
+    },
+    { 
+        name: "Ernesto Jonan",
+        avatar: "https://avatars0.githubusercontent.com/u/69017629?s=400&u=0dd09a9dd757b1c058bd89aedc32a638ff9c1d69&v=4",
+        whatsapp:"54654461665",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject:"Química",
+        cost:"15",
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220] 
+    },
+    { 
+        name: "Mayk Brito",
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        whatsapp:"54654461665",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject:"Química",
+        cost:"15",
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220] 
+    }
+]
